ca7b1849740bad475768a0f83978c9c6e676ac98
abradle <you@example.com>
2018-06-12 10:45:43 +0100

    Decharge input

--- frag/utils/network_utils.py
+++ frag/utils/network_utils.py
@@ -10,6 +10,56 @@ import os
 
 SMARTS_PATTERN = "[*;R]-;!@[*]"
 
+""" contribution from Hans de Winter """
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+
+def _InitialiseNeutralisationReactions():
+    patts = (
+        # Imidazoles
+        ("[n+;H]", "n"),
+        # Amines
+        ("[N+;!H0]", "N"),
+        # Carboxylic acids and alcohols
+        ("[$([O-]);!$([O-][#7])]", "O"),
+        # Thiols
+        ("[S-;X1]", "S"),
+        # Sulfonamides
+        ("[$([N-;X2]S(=O)=O)]", "N"),
+        # Enamines
+        ("[$([N-;X2][C,N]=C)]", "N"),
+        # Tetrazoles
+        ("[n-]", "[nH]"),
+        # Sulfoxides
+        ("[$([S-]=O)]", "S"),
+        # Amides
+        ("[$([N-]C=O)]", "N"),
+    )
+    return [(Chem.MolFromSmarts(x), Chem.MolFromSmiles(y, False)) for x, y in patts]
+
+
+_reactions = None
+
+
+def NeutraliseCharges(smiles, reactions=None):
+    global _reactions
+    if reactions is None:
+        if _reactions is None:
+            _reactions = _InitialiseNeutralisationReactions()
+        reactions = _reactions
+    mol = Chem.MolFromSmiles(smiles)
+    replaced = False
+    for i, (reactant, product) in enumerate(reactions):
+        while mol.HasSubstructMatch(reactant):
+            replaced = True
+            rms = AllChem.ReplaceSubstructs(mol, reactant, product)
+            mol = rms[0]
+    if replaced:
+        return (Chem.MolToSmiles(mol, True), True)
+    else:
+        return (smiles, False)
+
 
 def write_results(input_dict):
     """
@@ -319,7 +369,10 @@ def add_child_and_edge(
 
 
 def canon_input(smi):
-    return Chem.MolToSmiles(Chem.MolFromSmiles(smi), isomericSmiles=True)
+    # Decharge in this step too
+    return NeutraliseCharges(
+        Chem.MolToSmiles(Chem.MolFromSmiles(smi), isomericSmiles=True)
+    )[0]
 
 
 def create_children(input_node, node_holder):
