d9da3fc54acd2d6603ad89badc5a3742822af3a0
Alan Christie <alan.christie@matildapeak.com>
2018-08-07 15:54:56 +0100

    - Now works with any size node/edge file   Limiting factor is memory for the attributes map

--- frag/network/scripts/colate_all.py
+++ frag/network/scripts/colate_all.py
@@ -27,33 +27,34 @@ def do_for_dir(input_dir):
             print('%s Processing %s...' % (datetime.now(), f_name))
         line_num = 0
         out_f = open(f_name.replace(".txt", ".csv"), "w")
-        for line in open(f_name).readlines():
+        with open(f_name) as in_f:
+            for line in in_f:
 
-            if LINE_REMINDER:
-                # Count the lines, simply to allow us
-                # to allow us to print a re-assuring update...
-                line_num += 1
-                if line_num % LINE_REMINDER == 0:
-                    print('%s ...%d' % (datetime.now(), line_num))
+                if LINE_REMINDER:
+                    # Count the lines, simply to allow us
+                    # to allow us to print a re-assuring update...
+                    line_num += 1
+                    if line_num % LINE_REMINDER == 0:
+                        print('%s ...%d' % (datetime.now(), line_num))
 
-            line_spl = line.split()
-            out_l = []
-            for i, x in enumerate(prop_dict[f_name]):
-                if x:
-                    out_l.append(line_spl[i])
-            if f_name == "nodes.txt":
-                attr_cmp_id = attrs.get(line_spl[1], None)
-                if attr_cmp_id:
-                    cmpd_id = attr_cmp_id
-                    out_l.append(cmpd_id)
-                    # This is where we can add tags - like CHEAP - EXPENSIVE
-                    out_l.append("EM;MOL;F2")
-                else:
-                    out_l.append("")
-                    out_l.append("F2")
-                out_f.write(",".join(out_l) + "\n")
-            elif f_name == "edges.txt":
-                out_f.write(",".join(out_l) + "\n")
+                line_spl = line.split()
+                out_l = []
+                for i, x in enumerate(prop_dict[f_name]):
+                    if x:
+                        out_l.append(line_spl[i])
+                if f_name == "nodes.txt":
+                    attr_cmp_id = attrs.get(line_spl[1], None)
+                    if attr_cmp_id:
+                        cmpd_id = attr_cmp_id
+                        out_l.append(cmpd_id)
+                        # This is where we can add tags - like CHEAP - EXPENSIVE
+                        out_l.append("EM;MOL;F2")
+                    else:
+                        out_l.append("")
+                        out_l.append("F2")
+                    out_f.write(",".join(out_l) + "\n")
+                elif f_name == "edges.txt":
+                    out_f.write(",".join(out_l) + "\n")
 
         if LINE_REMINDER:
             print('%s Processed.' % datetime.now())
