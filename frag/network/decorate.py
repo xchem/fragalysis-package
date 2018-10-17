130dc6ec6344c9a08bdb05e220bbc92dc4883ee2
reskyner <reskyner@googlemail.com>
2018-10-17 14:06:52 +0100

    fix isolabels option on get_3d_vects_for_mol

--- frag/network/decorate.py
+++ frag/network/decorate.py
@@ -88,8 +88,8 @@ def deletion_linker_sd(input_mol, iso_labels=True):
     return deletion_linker_mol(mol, iso_labels)
 
 
-def get_3d_vects_for_mol(input_mol):
-    tot_dict = del_link_coord(input_mol, iso_labels=True)
+def get_3d_vects_for_mol(input_mol, iso_labels=True):
+    tot_dict = del_link_coord(input_mol, iso_labels)
     tot_dict["additions"] = decorate_3d_mol(input_mol)
     return tot_dict
 
