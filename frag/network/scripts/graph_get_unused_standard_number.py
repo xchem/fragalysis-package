90505c12b3a688099450d5228c88a7d8c28c1498
Alan Christie <alan.christie@matildapeak.com>
2019-03-07 08:43:24 +0000

    - Adds get-unused-standard-number - Bug-fix env error

--- frag/network/scripts/graph_get_unused_standard_number.py
+++ frag/network/scripts/graph_get_unused_standard_number.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-"""A utility to get the next available build number for a build path.
-The output of the utility is the next build number, i.e. '2'.
+"""A utility to get the next available standard number for a standard path.
+The output of the utility is the next standard number, i.e. '2'.
 
 To use this utility you will need to ensure that your AWS credentials
 are available via expected environment variables. Please refer to
@@ -22,16 +22,16 @@ import boto3
 
 # Expected environment variables (that define the bucket)
 s3_bucket_env = 'FRAGALYSIS_S3_BUCKET'
-s3_build_root = 'build'
+s3_standard_root = 'standard'
 s3_archive_bucket = os.environ.get(s3_bucket_env)
 if not s3_archive_bucket:
     print('You must define %s' % s3_bucket_env)
     sys.exit(1)
 
-parser = argparse.ArgumentParser('Graph Build Number Getter')
+parser = argparse.ArgumentParser('Graph Standard Number Getter')
 parser.add_argument('path', metavar='PATH', type=str,
                     help='The path build in your S3 bucket where'
-                         ' you expect the build to be placed'
+                         ' you expect the standard to be placed'
                          ' e.g. "activity/senp7"')
 
 args = parser.parse_args()
@@ -43,18 +43,18 @@ next_free_number = 1
 found = False
 
 # The S3 path path...
-dst = s3_build_root + '/' + args.path + '/'
+dst = s3_standard_root + '/' + args.path + '/'
 
 # List objects at the destination.
 # This will include things like
-# 'build/activity/senp7/build-1/nodes.txt.gz'.
-# For each key, look for a 'build' and then inspect its number.
+# 'standard/activity/senp7/standard-1/standardised-compounds.tab.gz'.
+# For each key, look for a 'standard' and then inspect its number.
 target = s3_client.list_objects_v2(Bucket=s3_archive_bucket,
                                    Prefix=dst)
 for content in target['Contents']:
     fields = content['Key'].split('/')
     for field in fields:
-        if field.startswith('build-'):
+        if field.startswith('standard-'):
             number = int(field.split('-')[1])
             if number >= next_free_number:
                 next_free_number = number + 1
