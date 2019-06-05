f14e502b16910e32bd2c7078420cdae46dcd7789
Alan Christie <alan.christie@matildapeak.com>
2019-06-05 16:55:56 +0100

    - An experiment with raw data collection

--- frag/network/scripts/graph_collect_raw_molport_data.py
+++ frag/network/scripts/graph_collect_raw_molport_data.py
@@ -53,8 +53,8 @@ if not s3_archive_bucket:
     logger.error('You must define %s', s3_bucket_env)
     sys.exit(1)
 
-collect_username = os.envrion.get('FRAGALYSIS_COLLECT_USERNAME')
-collect_password = os.envrion.get('FRAGALYSIS_COLLECT_PASSWORD')
+collect_username = os.environ.get('FRAGALYSIS_COLLECT_USERNAME')
+collect_password = os.environ.get('FRAGALYSIS_COLLECT_PASSWORD')
 if not collect_username or not collect_password:
     logger.error('You must define a username and password')
     sys.exit(1)
