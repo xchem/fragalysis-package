aad2d1b0a212c2f1a291e6d1d82ce033b876dafe
Alan Christie <alan.christie@matildapeak.com>
2019-07-31 13:06:16 +0100

    - Fix loader generation for node & edge header files

--- frag/network/scripts/graph_create_loader_script_from_directory.py
+++ frag/network/scripts/graph_create_loader_script_from_directory.py
@@ -2,10 +2,11 @@
 # coding=utf-8
 
 """A utility to create the Graph loader script from the files in a named
-directory.
+directory. This is generally employed when combining data sets,
+where 2 or more graph databases are combined.
 
 Alan Christie
-May 2019
+July 2019
 """
 
 import argparse
@@ -49,19 +50,28 @@ if not os.path.isdir(args.path):
 
 # Just construct a dictionary of nodes and edges based on the
 # directory content. We add every file that contains the word
-# 'nodes' or 'edges'.
+# 'nodes' or 'edges' and ends in '.gz'.
+#
+# If the file is 'nodes.csv.gz' or 'edges.csv.gz' we expect a header
+# and instead of just adding the file to the list we also add the header.
+# So, if we find 'nodes.csv.gz' we insert 'nodes-header.csv,nodes.csv.gz'
+# into the corresponding list.
 generated_files = {'nodes': [],
                    'edges': []}
 possible_files = os.listdir(args.path)
 for possible_file in possible_files:
-    if 'edges' in possible_file:
-        file_path = os.path.join(args.path, possible_file)
-        if os.path.isfile(file_path):
-            generated_files['edges'].append(possible_file)
-    elif 'nodes' in possible_file:
-        file_path = os.path.join(args.path, possible_file)
-        if os.path.isfile(file_path):
-            generated_files['nodes'].append(possible_file)
+    if possible_file.endswith('.gz') and \
+            os.path.isfile(os.pth.join(args.path, possible_file)):
+        if 'edges' in possible_file:
+            actual_file = possible_file
+            if actual_file == 'edges.csv.gz':
+                actual_file = 'edges-header.csv,edges.csv.gz'
+            generated_files['edges'].append(actual_file)
+        elif 'nodes' in possible_file:
+            actual_file = possible_file
+            if actual_file == 'nodes.csv.gz':
+                actual_file = 'nodes-header.csv,nodes.csv.gz'
+            generated_files['nodes'].append(actual_file)
 
 if not generated_files['nodes']:
     error('Could not find any nodes files.')
